1506d5b8bbf939ed5b6d0558aed8c266cfcf625d
AvigailShlomov <anm798@gmail.com>
2024-12-03 10:36:07 +0200

    new rate chart comp

---
+++ src/app/components/rate-chart/rate-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rate-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './rate-chart.component.html',
+  styleUrl: './rate-chart.component.css'
+})
+export class RateChartComponent {
+
+}
