81ed513d9c4a8444ca9b493948c0c1bc4f7a2f87
AvigailShlomov <anm798@gmail.com>
2024-12-03 10:38:25 +0200

    add init vars

---
+++ src/app/components/converter/converter.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-converter',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './converter.component.html',
+  styleUrl: './converter.component.css'
+})
+export class ConverterComponent {
+  amount: number = 1;
+  fromCurrency: string = "ILS";
+  toCurrency: string = "USD";
+  result: number | null = null;
+  currencies:string[]=["ILS",'EUR', 'USD', 'GBP', 'JPY', 'AUD', 'CAD', 'CHF', 'CNY'];
+  
+}
