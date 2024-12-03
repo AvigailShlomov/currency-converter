d3cff78fbb93b472f0673bdb0923e8b4d3a501a9
AvigailShlomov <anm798@gmail.com>
2024-12-03 18:23:08 +0200

    add HistoryComponent to app for now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ConverterComponent } from './components/converter/converter.component';
+import { HistoryComponent } from './components/history/history.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ConverterComponent],
+  imports: [RouterOutlet, ConverterComponent, HistoryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
