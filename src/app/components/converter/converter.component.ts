c554f5beae7383440f94db9a963150239b28d647
AvigailShlomov <anm798@gmail.com>
2024-12-03 14:08:17 +0200

    new convert method and DI

--- src/app/components/converter/converter.component.ts
+++ src/app/components/converter/converter.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CurrencyService } from '../../services/currency.service';
 
 @Component({
   selector: 'app-converter',
@@ -16,4 +17,14 @@ export class ConverterComponent {
   result: number | null = null;
   currencies: string[] = ["ILS", 'EUR', 'USD', 'GBP', 'JPY', 'AUD', 'CAD', 'CHF', 'CNY'];/**@todo: get currencies List from api */
 
+  constructor(private currerncyService: CurrencyService) { }
+
+  convert() {
+    this.currerncyService.convertCurrency(this.fromCurrency, this.toCurrency, this.amount)
+      .subscribe({
+        next: ((res) => this.result = res.rates[this.toCurrency]),
+        error: ((err) => alert("Error Wilte fetching currency conversion"))
+      })
+    return;
+  }
 }
