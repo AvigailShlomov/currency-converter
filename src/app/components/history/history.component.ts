8174935bcadba82a54bc8bd029ce38a72d513b79
AvigailShlomov <anm798@gmail.com>
2024-12-03 18:23:27 +0200

    add base hisoty data

--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../../services/storage.service';
+import { ConversionForStorage } from '../../models/currency.models';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-history',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './history.component.html',
   styleUrl: './history.component.css'
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit {
+  conversionHistory: ConversionForStorage[] = [];
+
+  constructor(private storageService: StorageService) { }
+
+  ngOnInit() {
+    this.conversionHistory = this.storageService.getHistory();
+  }
 
 }
