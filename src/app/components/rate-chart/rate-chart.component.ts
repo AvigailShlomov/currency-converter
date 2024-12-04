7714e22cccbbd6bfabec17d9fdc5c364b07acc5a
AvigailShlomov <anm798@gmail.com>
2024-12-04 10:14:01 +0200

    start getChartData from  service

--- src/app/components/rate-chart/rate-chart.component.ts
+++ src/app/components/rate-chart/rate-chart.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CurrencyService } from '../../services/currency.service';
+import { HistoricalRates } from '../../models/currency.models';
 
 @Component({
   selector: 'app-rate-chart',
@@ -7,6 +9,29 @@ import { Component } from '@angular/core';
   templateUrl: './rate-chart.component.html',
   styleUrl: './rate-chart.component.css'
 })
-export class RateChartComponent {
+export class RateChartComponent implements OnInit {
+  historicalRates: HistoricalRates | null = null;/**@may be not null */
+  constructor(private currencyService: CurrencyService) { }
+
+  ngOnInit() {
+    this.getChartData();
+  }
+
+  getChartData() {
+    this.currencyService.getHistoricalRates("ILS", "USD").subscribe({
+      next:( (res)=>{
+        // this.historicalRates={
+        //   base:res.base,
+        //   start_date:res.start_date,
+        //   end_date:res.end_date,
+         
+        // }
+      }
+
+      )
+    })
+
+  }
+
 
 }
