499528ed6b89168c85c5e95630d467e4d34ab762
AvigailShlomov <anm798@gmail.com>
2024-12-04 09:42:17 +0200

    add pastWeekDateRange calc func

--- src/app/services/currency.service.ts
+++ src/app/services/currency.service.ts
@@ -9,9 +9,24 @@ import { converterResponse } from '../models/currency.models';
 export class CurrencyService {
   private baseUrl = "https://api.frankfurter.dev/v1";
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  convertCurrency(from: string, to: string, amount: number) :Observable<converterResponse> {  
+  convertCurrency(from: string, to: string, amount: number): Observable<converterResponse> {
     return this.http.get<any>(`${this.baseUrl}/latest?amount=${amount}&base=${from}&symbols=${to}`);
-   }
+  }
+
+  getHistoricalRates() {
+    const pastWeekDateRange= this.pastWeekDateRange();
+
+
+  }
+  //https://api.frankfurter.dev/v1/2024-01-01..2024-01-02?symbols=USD&base=ILS
+
+  /**@todo: maybe move to utils */
+  private pastWeekDateRange(): { todayDate: Date, lastWeekDate: Date } {
+    const todayDate = new Date();
+    let lastWeekDate = new Date();
+    lastWeekDate.setDate(todayDate.getDate() - 7);
+    return { todayDate, lastWeekDate };
+  }
 }
