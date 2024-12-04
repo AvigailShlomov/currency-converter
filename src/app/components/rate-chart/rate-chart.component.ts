b447dbb36506dd6385d063d69be797c786cbbf9c
AvigailShlomov <anm798@gmail.com>
2024-12-04 15:23:53 +0200

    change chart fonnow

--- src/app/components/rate-chart/rate-chart.component.ts
+++ src/app/components/rate-chart/rate-chart.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CurrencyService } from '../../services/currency.service';
 import { HistoricalRates } from '../../models/currency.models';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-rate-chart',
@@ -10,28 +11,32 @@ import { HistoricalRates } from '../../models/currency.models';
   styleUrl: './rate-chart.component.css'
 })
 export class RateChartComponent implements OnInit {
-  historicalRates: HistoricalRates | null = null;/**@may be not null */
+  // @Input() fromCurrency: string=""; /**@todo: is this ok? */
+  // @Input() toCurrency: string="";
+  historicalRates: HistoricalRates | null = null;/**@todo: maybe not null */
   constructor(private currencyService: CurrencyService) { }
 
   ngOnInit() {
-    this.getChartData();
+    // this.getChartData();
   }
 
-  getChartData() {
-    this.currencyService.getHistoricalRates("ILS", "USD").subscribe({
-      next:( (res)=>{
-        // this.historicalRates={
-        //   base:res.base,
-        //   start_date:res.start_date,
-        //   end_date:res.end_date,
+  // getChartData() {
+  //   this.currencyService.getHistoricalRates(this.fromCurrency, this.toCurrency).subscribe({
+  //     next:( (res)=>{
+  //       console.log("rate chart",res);
+        
+  //       // this.historicalRates={
+  //       //   base:res.base,
+  //       //   start_date:res.start_date,
+  //       //   end_date:res.end_date,
          
-        // }
-      }
+  //       // }
+  //     }
 
-      )
-    })
+  //     )
+  //   })
 
-  }
+  // }
 
 
 }
